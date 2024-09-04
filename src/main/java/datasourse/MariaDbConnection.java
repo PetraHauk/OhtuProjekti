d18a0dd402fb63a18baa1399ba6a2057204328a8
Petra Haukkala <petra.haukkala@gmail.com>
2024-09-04 12:52:59 +0300

    MariaDB (Toimiiko?)

---
+++ src/main/java/datasourse/MariaDbConnection.java
@@ -0,0 +1,34 @@
+package datasourse;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MariaDbConnection {
+
+    private static Connection conn = null;
+    public static Connection getConnection() {
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = DriverManager.getConnection(
+                        "jdbc:mariadb://localhost:3306/hotelli_sgl?user=root&password=" + System.getenv("DB_PASSWORD"));
+            }
+            return conn;
+        } catch (SQLException e) {
+            System.out.println("Connection failed.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void terminate() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
