5a25f60562f28b907904dfd7695f831cbcf29626
annagaom <122282732+annagaom@users.noreply.github.com>
2024-09-05 12:42:32 +0300

    uusi AsiakasDao

--- src/main/java/model/DAO/AsiakasDAO.java
+++ src/main/java/model/DAO/AsiakasDAO.java
@@ -1,4 +1,14 @@
 package model.DAO;
 
+import model.datasourse.MariaDbConnection;
+import model.enteties.Asiakas;
+import jakarta.persistence.EntityManager;
+
 public class AsiakasDAO {
+    public void persist(Asiakas asiakas) {
+        EntityManager em = MariaDbConnection.terminate();
+        em.getTransaction().begin();
+        em.persist(asiakas);
+        em.getTransaction().commit();
+    }
 }
