adba5791dc911c8c80bcf6c4d227424b4316938a
annagaom <122282732+annagaom@users.noreply.github.com>
2024-09-17 14:17:50 +0300

    asiakas-taulukon kanssa toiminnat valmis

--- src/main/java/app/AnnaMain.java
+++ src/main/java/app/AnnaMain.java
@@ -4,11 +4,23 @@ public class AnnaMain {
 
 
     public static void main(String[] args) {
-        KayttajaHaku kayttajaHaku = new KayttajaHaku();
-        kayttajaHaku.start();
+        //KayttajaHaku kayttajaHaku = new KayttajaHaku();
+        //kayttajaHaku.start();
 
         //HotelliHaku hotelliHaku = new HotelliHaku();
         //hotelliHaku.start();
+
+        //HuoneHaku huoneHaku = new HuoneHaku();
+        //huoneHaku.start();
+
+        //VarausHaku varausHaku = new VarausHaku();
+        //varausHaku.start();
+
+        //LaskuHaku laskuHaku = new LaskuHaku();
+        //laskuHaku.start();
+
+        AsiakasHaku asiakasHaku = new AsiakasHaku();
+        asiakasHaku.start();
     }
 
 }
