dfc85eceacfd1a4bff85909dc9c4903a1f85ef4a
annagaom <122282732+annagaom@users.noreply.github.com>
2024-09-28 18:56:15 +0300

    HuoneController test valmis

---
+++ src/src/test/java/controller/HuoneControllerTest.java
@@ -0,0 +1,74 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HuoneControllerTest {
+    HuoneController huoneController = new HuoneController();
+
+    @Test
+    public void testLisaaHuone() {
+        int huoneNro = 110;
+        String huoneTyyppi = "perhe";
+        String huoneTila = "vapaa";
+        double hinta = 100.0;
+        int hotelliId = 2;
+        huoneController.lisaaHuone(huoneNro, huoneTyyppi, huoneTila, hinta, hotelliId);
+        assertEquals(huoneNro, huoneController.findHuoneByNro(huoneNro).getHuone_nro());
+    }
+
+    @Test
+    public void testFindHuoneById() {
+        int huoneId = 1;
+        huoneController.findHuoneById(huoneId);
+        assertEquals(huoneId, huoneController.findHuoneById(huoneId).getHuone_id());
+    }
+
+    @Test
+    public void testFindHuoneetByHoteliId() {
+        int hotelliId = 1;
+        huoneController.findHuoneById(hotelliId);
+        assertEquals(hotelliId, huoneController.findHuoneById(hotelliId).getHotelli_id());
+    }
+
+    @Test
+    public void findHuoneByNro() {
+        int huoneNro = 101;
+        huoneController.findHuoneByNro(huoneNro);
+        assertEquals(huoneNro, huoneController.findHuoneByNro(huoneNro).getHuone_nro());
+    }
+
+    @Test
+    public void findHuoneByTila() {
+        String huoneTila = "vapaa";
+        huoneController.findHuoneByTila(huoneTila);
+        assertNotNull(huoneController.findHuoneByTila(huoneTila));
+    }
+
+    @Test
+    public void findHuoneByTyyppi() {
+        String huoneTyyppi = "perhe";
+        huoneController.findHuoneByTyyppi(huoneTyyppi);
+        assertNotNull(huoneController.findHuoneByTyyppi(huoneTyyppi));
+    }
+
+    @Test
+    public void updateHuoneById() {
+        int huoneId = 6;
+        int huoneNro = 303;
+        String huoneTyyppi = "sviittihe";
+        String huoneTila = "varattu";
+        double hinta = 350.0;
+        huoneController.updateHuoneById(huoneId, huoneNro, huoneTyyppi, huoneTila, hinta);
+        assertEquals(huoneNro, huoneController.findHuoneById(huoneId).getHuone_nro());
+    }
+
+    @Test
+    public void deleteHuone() {
+        int huoneId = 7;
+        huoneController.deleteHuone(huoneId);
+        assertNull(huoneController.findHuoneById(huoneId));
+    }
+
+}
