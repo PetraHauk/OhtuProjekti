65d7ab5f21e87fe54ed8dec60d0768def1d7f08e
kake <kaisla.ketola@metropolia.fi>
2024-10-04 22:37:45 +0300

    muuttaa pomin ja dockerfilen docker pull ja buildia varten!

---
+++ src/src/test/java/controller/kayttajaControllerTest.java
@@ -0,0 +1,16 @@
+//package controller;
+//
+//import org.testng.annotations.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class KayttajaControllerTest {
+//
+//    KayttajaController kayttajaController = new KayttajaController();
+//
+//    @Test
+//    public void testFindKayttajaByI() {
+//        int id = 2;
+//        assertEquals(2, kayttajaController.haeKayttajaById(2).getKayttajaId());
+//    }
+//}
